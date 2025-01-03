5c7acbe2563f5a8c0d799125326d5c023ef6ecbe
anshbhalala2007 <bhalalaansh016@gmail.com>
2025-01-03 15:12:42 +0530

    extra_pr_3.c

--- extra_pr_3.c
+++ extra_pr_3.c
@@ -4,30 +4,30 @@
 int main() {
     int num, originalNum, remainder, n = 0, result = 0;
 
-    // Input the number from the user
+    
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Store the original number to compare later
+    
     originalNum = num;
 
-    // Find the number of digits in the number
+    
     while (num != 0) {
-        num /= 10;  // Remove the last digit
-        ++n;  // Count the number of digits
+        num /= 10;  
+        ++n;  
     }
 
-    // Reset num to the original number
+    
     num = originalNum;
 
-    // Calculate the sum of each digit raised to the power of n
+    
     while (num != 0) {
-        remainder = num % 10;  // Get the last digit
-        result += pow(remainder, n);  // Add the nth power of the digit
-        num /= 10;  // Remove the last digit
+        remainder = num % 10;  
+        result += pow(remainder, n);  
+        num /= 10;  
     }
 
-    // Check if the sum is equal to the original number
+    
     if (result == originalNum) {
         printf("%d is an Armstrong number.\n", originalNum);
     } else {
