5e11f7d7eff013dcc82ca731c56e07b9a89248fb
anshbhalala2007 <bhalalaansh016@gmail.com>
2025-01-03 15:15:10 +0530

    U_extra_pr_6.c

--- extra_pr_6.c
+++ extra_pr_6.c
@@ -2,27 +2,27 @@
 #include <string.h>
 #include <math.h>
 
-// Function to convert binary to octal
+
 void binaryToOctal(char binary[]) {
     int length = strlen(binary);
 
-    // Make the length of the binary string a multiple of 3 by adding leading zeros
+   
     int padding = 3 - (length % 3);
     if (padding != 3) {
-        // Shift the binary string to the left by adding the required number of leading zeros
-        memmove(binary + padding, binary, length + 1); // Shift original string
+        
+        memmove(binary + padding, binary, length + 1); 
         for (int i = 0; i < padding; i++) {
-            binary[i] = '0';  // Add leading zeros
+            binary[i] = '0'; 
         }
     }
 
-    // Now process the binary string in chunks of 3
+   
     printf("Octal equivalent: ");
     for (int i = 0; i < strlen(binary); i += 3) {
-        // Convert each 3-bit chunk to its octal equivalent
+        
         int decimal = 0;
         for (int j = 0; j < 3; j++) {
-            decimal += (binary[i + j] - '0') * pow(2, 2 - j); // Convert binary chunk to decimal
+            decimal += (binary[i + j] - '0') * pow(2, 2 - j); 
         }
         printf("%d", decimal);
     }
@@ -32,11 +32,10 @@ void binaryToOctal(char binary[]) {
 int main() {
     char binary[100];
 
-    // Input binary number from the user
+
     printf("Enter a binary number: ");
     scanf("%s", binary);
 
-    // Convert binary to octal
     binaryToOctal(binary);
 
     return 0;
