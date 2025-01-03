94d86eaac3b860d86263953824359d20f4a6afdc
anshbhalala2007 <bhalalaansh016@gmail.com>
2025-01-03 15:15:38 +0530

    U_extra_pr_7.c

--- extra_pr_7.c
+++ extra_pr_7.c
@@ -3,17 +3,17 @@
 int main() {
     long long n, m, a;
 
-    // Input the dimensions of the square and flagstone
+    
     scanf("%lld %lld %lld", &n, &m, &a);
 
-    // Calculate the number of flagstones needed for each dimension
+  
     long long num_flagstones_width = (n + a - 1) / a;
     long long num_flagstones_height = (m + a - 1) / a;
 
-    // Total number of flagstones is the product of the two
+   
     long long total_flagstones = num_flagstones_width * num_flagstones_height;
 
-    // Output the result
+
     printf("%lld\n", total_flagstones);
 
     return 0;
